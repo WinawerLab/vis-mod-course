ea10d46449e8c9503e89a2329631798cbb4fd56b
elinekupers <elinekupers@gmail.com>
2018-06-04 15:58:10 -0400

    match mean luminance and illuminance to paper

--- Banks1991/functions/getSceneParams.m
+++ Banks1991/functions/getSceneParams.m
@@ -5,5 +5,7 @@ function [sparams, fov2deg] = getSceneParams()
 sparams.fov       = 1;   % scene field of view in degrees (diameter)
 fov2deg           = 1/sparams.fov;
 sparams.distance  = 0.57;  % meters
+sparams.illuminantphotons = 1348 * 10; % retinal illumance, photopic Td. Transformed to photons by multiplying by 10.
+sparams.meanluminance = 762; % cd/m2
 
 return
